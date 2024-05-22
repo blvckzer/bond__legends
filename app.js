5f9d7fe51eb8f725809a4372561faa103f4d9828
blvckzer <adhuzadhuz6@gmail.com>
2024-05-22 23:34:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,56 +23,56 @@ let curr_track = document.createElement('audio');
 let track_list = [
     {
     name: "Ranjha Musical Cover",
-    artist: "Hanan Shaah Ft Jazeem & Ibnu Azru",
-    image: "https://i.ytimg.com/vi/OJqt-SUFENg/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m8.mp3"
   },
   {
     name: "Mehabooba Musical Cover ",
-    artist: "Hanan Shaah Ft Fajish",
-    image: "https://i.ytimg.com/vi/ZeTihwknnAc/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m9.mp3"
   },
   {
     name: "Painkiller Songs|pain killers for love failures | soulful songs 8D",
-    artist: "Sid Sriram",
-    image: "https://i.ytimg.com/vi/92d1iKfScGM/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m1.mp3"
   },
   {
     name: "Porkanda singam",
-    artist: "Anirudh Ravichandhar",
-    image: "https://i.ytimg.com/vi/5yDZXNSDAsg/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m2.mp3"
   },
   {
     name: "8d Thattathin Marayath All Songs",
-    artist: "nivin pauly | isha thalwar | vineeth sreenivasan | shan rahman |",
-    image: "https://i.ytimg.com/vi/p2OHWdyZBEk/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m3.mp3"
   },
   {
     name: "Anirudh Melody Hits - Best of Anirudh",
-    artist: "Anirudh Ravichandhar",
-    image: "https://i.ytimg.com/vi/zDStGFY5SZM/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m4.mp3"
   },
   {
     name: "Sid Sriram Melody Hits",
-    artist: "Sid Sriram",
-    image: "https://i.ytimg.com/vi/-aHmVsJV64c/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m5.mp3"
   },
   {
     name: "Othai Thamarai",
-    artist: "Nixen, Bala, Sandy Sandellow",
-    image: "https://i.ytimg.com/vi/JZBp0nEzFo8/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m6.mp3"
   },
   {
     name: "Don - Bae Song",
-    artist: "Sivakarthikeyan, Priyanka Mohan | Anirudh Ravichander",
-    image: "https://i.ytimg.com/vi/-7n_krPLRgs/0.jpg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    artist: "𝐁𝐎𝐍𝐃 𝐋𝐄𝐆𝐄𝐍𝐃𝐒",
+    image: "https://i.imgur.com/v14Dfao.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "./ktb/m7.mp3"
   },   
 ];
